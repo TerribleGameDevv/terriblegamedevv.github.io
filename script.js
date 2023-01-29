1730c2331533e86221bdcb55016d787de182b089
TerribleGameDevv <123600759+TerribleGameDevv@users.noreply.github.com>
2023-01-29 00:53:11 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
 const API_KEY = "AIzaSyBbhju011EtzN-elXRz_wtDfaaBI1T3DNo";
-const convertBtn = document.getElementById("convert-btn");
-const player = document.getElementById("player");
-const linkInput = document.getElementById("youtube-link");
+const convertBtn = $("#convert-btn");
+const player = $("#player");
+const linkInput = $("#youtube-link");
 
-convertBtn.addEventListener("click", convert);
-
-function convert(event) {
+convertBtn.click(function(event) {
   event.preventDefault();
-  const link = linkInput.value;
+  const link = linkInput.val();
   if(!link){
     alert("Please enter a YouTube link");
     return;
   }
-  fetch("https://yourapi.com/convert?link=" + link)
-    .then(response => response.json())
-    .then(data => {
-      const video = document.createElement("video");
-      video.src = data.mp4Url;
-      video.controls = true;
-      player.appendChild(video);
-    })
-    .catch(error => console.error(error));
-}
+  $.getJSON("https://yourapi.com/convert?link=" + link, function(data) {
+    const video = $("<video>", {
+      src: data.mp4Url,
+      controls: true
+    });
+    player.append(video);
+  });
+});
