f354055926b8a2466e4ee0149831f0ae6cd5ad16
TerribleGameDevv <123600759+TerribleGameDevv@users.noreply.github.com>
2023-01-29 00:49:08 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 const API_KEY = "AIzaSyBbhju011EtzN-elXRz_wtDfaaBI1T3DNo";
+const convertBtn = document.getElementById("convert-btn");
+const player = document.getElementById("player");
+const linkInput = document.getElementById("youtube-link");
 
-function convert() {
-  const link = document.getElementById("link").value;
-  const videoId = link.split("v=")[1];
-  const url = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${API_KEY}&part=snippet,contentDetails,statistics,status`;
+convertBtn.addEventListener("click", convert);
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      const mp4Url = data.items[0].contentDetails.videoId;
-      const player = document.getElementById("player");
-      player.innerHTML = `
-        <video controls>
-          <source src="${mp4Url}" type="video/mp4">
-          Your browser does not support HTML5 video.
-        </video>
-      `;
-    });
+function convert(event) {
+  event.preventDefault();
+  const link = linkInput.value;
+  if(!link){
+    alert("Please enter a YouTube link");
+    return;
+  }
+  fetch("https://yourapi.com/convert?link=" + link)
+    .then(response => response.json())
+    .then(data => {
+      const video = document.createElement("video");
+      video.src = data.mp4Url;
+      video.controls = true;
+      player.appendChild(video);
+    })
+    .catch(error => console.error(error));
 }
